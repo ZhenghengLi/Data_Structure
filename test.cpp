089692764fd2cf075b3c6c94cf407bd78df98307
Zhengheng Li <zhenghenge@gmail.com>
2015-02-20 22:24:09 +0800

    second commit

--- test.cpp
+++ test.cpp
@@ -2,24 +2,26 @@
 
 using namespace std;
 
-void moveDisks(int count, int needle1, int needle3, int needle2)
-{
-	if(count > 0)
-	{
-		moveDisks(count - 1, needle1, needle2, needle3);
-		cout << "Move disk " << count << " from " << needle1
-			 << " to " << needle3 << "." << endl;
-		moveDisks(count-1, needle2, needle3, needle1);
-	}
-}
+void decToBin(int num, int base);
 
 int main(int argc, char** argv)
 {
-	int n;
-	cout << "n = ";
-	cin >> n;
-	moveDisks(n,1,3,2);
+	int decimalNum;
+	int base;
+	base = 2;
+	cout << "Enter number in decimal: ";
+	cin >> decimalNum;
+	cout << "Decimal " << decimalNum << " = ";
+	decToBin(decimalNum, base);
+	cout << " binary" << endl;
 	return 0;
 }
 
-
+void decToBin(int num, int base)
+{
+	if (num > 0)
+	{
+		decToBin(num / base, base);
+		cout << num % base;
+	}
+}

83509c43ce6a6d0419a2eb5dc2768be611432786
Zhengheng Li <zhenghenge@gmail.com>
2015-02-22 19:03:33 +0800

    nQueensPuzzle

--- test.cpp
+++ test.cpp
@@ -1,27 +1,10 @@
-#include <iostream>
+#include "queens.h"
 
 using namespace std;
 
-void decToBin(int num, int base);
-
-int main(int argc, char** argv)
+int main()
 {
-	int decimalNum;
-	int base;
-	base = 2;
-	cout << "Enter number in decimal: ";
-	cin >> decimalNum;
-	cout << "Decimal " << decimalNum << " = ";
-	decToBin(decimalNum, base);
-	cout << " binary" << endl;
+	nQueensPuzzle queens(8);
+	queens.queensConfiguration(0);
 	return 0;
-}
-
-void decToBin(int num, int base)
-{
-	if (num > 0)
-	{
-		decToBin(num / base, base);
-		cout << num % base;
-	}
 }
